6894c6b692184a5ef560897dcbbc9ac36a51b34d
Autran Francisco Martine Castão <autran.castao@hiplatform.com>
2023-09-30 18:18:11 -0300

    [WIP] - Criação do projeto

---
+++ FiapNews/Dominio/Entidades/Noticia.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Dominio.Entidades
+{
+    public class Noticia : Base
+    {
+        //public string Title { get; private set; }
+        //public string Subtitle { get; private set; }
+        //public string Content { get; private set; }
+        //public string Lead { get; private set; }
+        //public List<string> Images { get; private set; }
+        ////public List<Comment> Comments { get; private set; }
+        //public Guid AuthorId { get; private set; }
+        ////public virtual Author Author { get; private set; }
+        //public DateTime CreatedAt { get; private set; }
+        ////public List<News> RelatedNews { get; private set; }
+        //public List<string> Tags { get; private set; }
+        ////public Region Region { get; private set; }
+
+        //public News(string title, string subtitle, string content, string lead, List<string> images,
+        //            Guid authorId, List<News> relatedNews, List<string> tags, Region region)
+        //{
+        //    Id = Guid.NewGuid();
+        //    Title = title;
+        //    Subtitle = subtitle;
+        //    Content = content;
+        //    Lead = lead;
+        //    Images = images ?? new List<string>();
+        //    Comments = new List<Comment>();
+        //    AuthorId = authorId;
+        //    CreatedAt = DateTime.UtcNow;
+        //    RelatedNews = relatedNews ?? new List<News>();
+        //    Tags = tags ?? new List<string>();
+        //    Region = region;
+        //}
+
+        //public void AddComment(string text, Guid userComented)
+        //{
+        //    Comments.Add(new Comment(text, userComented, Id));
+        //}
+    }
+}
