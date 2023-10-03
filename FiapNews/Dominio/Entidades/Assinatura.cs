ec22fd801f242586f6ec62648fcc46a2769b0945
rafael pontin <rfmpontin@gmail.com>
2023-10-02 21:32:28 -0300

    feat(Assinatura): Desenvolvimento do dominio da assinatura

---
+++ FiapNews/Dominio/Entidades/Assinatura.cs
@@ -0,0 +1,54 @@
+﻿using Dominio.Entidades.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entidades
+{
+    public class Assinatura : Base
+    {
+        public TipoAssinatura TipoAssinatura { get; private set; }
+        public double Preco { get; private set; }
+        public int TipoPlano { get; private set; }
+
+        public Assinatura(TipoAssinatura tipoAssinatura) : base()
+        {
+            AdicionaAssinatura(tipoAssinatura);
+        }
+
+        private void AdicionaAssinatura(TipoAssinatura tipoAssinatura)
+        {
+            TipoAssinatura = tipoAssinatura;
+            CalculaPreco();
+        }
+
+        private void CalculaPreco()
+        {
+            switch (TipoAssinatura)
+            {
+                case TipoAssinatura.BASICO: Preco = 0; break;
+                case TipoAssinatura.AUTOR:  Preco = 0; break;
+                case TipoAssinatura.ADMIN: Preco = 0; break;
+                case TipoAssinatura.PAGO: Preco = 50; break;
+                default: throw new ArgumentException("Não foi possivel encontrar Preço para o TipoAssinatura ", nameof(Preco));
+            }
+        }
+
+        private void CalculaPeriodicidade()
+        {
+            switch (TipoAssinatura) 
+            {
+                case TipoAssinatura.BASICO: TipoPlano = 360; break;
+                case TipoAssinatura.AUTOR:  TipoPlano = 360; break;
+                case TipoAssinatura.ADMIN:  TipoPlano = 360; break;
+                case TipoAssinatura.PAGO:   TipoPlano = 30; break;
+            }
+        }
+
+
+
+
+    }
+}
