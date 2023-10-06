5c4d9ce79c5fe47a58f5f5c8993ddc920bc9c9a0
alexssander <alexssanderferreira@hotmail.com>
2023-10-06 20:22:40 -0300

    [WIP] - Correçoes na entidade Comentario e deletado ComentarioPai.

--- FiapNews/Dominio/Entidades/Administrador.cs
+++ FiapNews/Dominio/Entidades/Administrador.cs
@@ -1,32 +1,14 @@
-<<<<<<< HEAD
+
+using Dominio.Enum;
+
 namespace Dominio.Entidades;
 
 public class Administrador : Usuario
 {
-    public Administrador()
-    {
-
-    }
     public Administrador(string nome, string login, string senha, string email, string foto)
-        : base(nome, login, senha, email, foto)
-=======
-using Dominio.Enum;
-
-namespace Dominio.Entidades
-{
-    public class Administrador : Usuario
->>>>>>> 805759053df6e8cd728b6ad97514d8199645f41f
+        : base(nome, login, senha, email, foto, TipoUsuario.ADMINISTRADOR)
     {
 
-<<<<<<< HEAD
-=======
-        }
-        public Administrador(string nome, string login, string senha, string email, string foto)
-            : base(nome, login, senha, email, foto, TipoUsuario.ADMINISTRADOR)
-        {
-
-        }
->>>>>>> 805759053df6e8cd728b6ad97514d8199645f41f
     }
 }
 
