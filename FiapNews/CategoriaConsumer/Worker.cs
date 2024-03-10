02bd7b718f8493ef0d7c081a9a7903d9c9e6935c
Elielson Rodrigues <elielson.dev@gmail.com>
2024-03-10 18:12:21 -0300

    Ajuste gravação config-site

--- FiapNews/CategoriaConsumer/Worker.cs
+++ FiapNews/CategoriaConsumer/Worker.cs
@@ -1,9 +1,7 @@
-using Aplicacao.DTOs;
 using CategoriaConsumer.request;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
-using System.Text.Json;
 
 namespace CategoriaConsumer;
 
@@ -11,55 +9,53 @@ public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
     private IRequest _request;
+    private readonly IConnection _connection;
+    private readonly IModel _channel;
 
     public Worker(ILogger<Worker> logger, IRequest request)
     {
         _logger = logger;
         _request = request;
+
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost",
+            UserName = "guest",
+            Password = "guest"
+        };
+        _connection = factory.CreateConnection();
+        _channel = _connection.CreateModel();
+        _channel.QueueDeclare(
+            queue: "fila-categoria",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+        );
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            var factory = new ConnectionFactory()
+            var consumer = new EventingBasicConsumer(_channel);
+
+            consumer.Received += (sender, eventArgrs) =>
             {
-                HostName = "localhost",
-                UserName = "guest",
-                Password = "guest"
+                var body = eventArgrs.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                _request.Post(message);
+                _channel.BasicAck(eventArgrs.DeliveryTag, false);
+
+                Console.WriteLine(message);
             };
 
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
-            {
-                channel.QueueDeclare(
-                    queue: "fila-categoria",
-                    durable: false,
-                    exclusive: false,
-                    autoDelete: false,
-                    arguments: null
-                );
-
-
-                var consumer = new EventingBasicConsumer(channel);
-
-                consumer.Received += (sender, eventArgrs) =>
-                {
-
-                    var body = eventArgrs.Body.ToArray();
-                    var message = Encoding.UTF8.GetString(body);
-                    _request.Post(message);
-
-                    Console.WriteLine(message);
-                };
-
-                channel.BasicConsume(
-                    queue: "fila-categoria",
-                    autoAck: true,
-                    consumer: consumer
-                );
-            }
-            await Task.Delay(5000);
+            _channel.BasicConsume(
+                queue: "fila-categoria",
+                autoAck: false,
+                consumer: consumer
+            );
         }
+        await Task.Delay(500);
     }
 }
